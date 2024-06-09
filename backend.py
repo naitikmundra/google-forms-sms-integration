23d05e391b708dfa6bab56d23191a1bfdb061103
naitikmundra <90164441+naitikmundra@users.noreply.github.com>
2024-06-09 17:53:46 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -3,14 +3,20 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
+def response(contact):
+    user_mobile_no = contact
+    #Send sms to user_mobile_no
+    pass
+    
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         data = request.json
         print(f"Received data: {data}")
+        response = data['message'][0] #Change 0 according to  the question no. for contact number in form.
+        sendsms(response)
         # Do something with the data here, e.g., store it in a database
-        return jsonify({'status': 'successfully', 'data': data}), 200
+        return jsonify({'status': 'successfully', 'data': "sent sms"}), 200
     else:
         return jsonify({'status': 'failure', 'message': 'Invalid request method'}), 400
 
