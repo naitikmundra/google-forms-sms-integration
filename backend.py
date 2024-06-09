f5446faaba3a9d46259fdc24bd3fe81b0feadd21
naitikmundra <90164441+naitikmundra@users.noreply.github.com>
2024-06-09 18:11:57 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,12 +1,25 @@
 #A post request recieving backend, made with python, just host the app(on pythonanywhere ie FREE, etc) and add its link + /webhook to the url in Code.gs file.
 
 from flask import Flask, request, jsonify
-
+import os
+from twilio.rest import Client
 app = Flask(__name__)
 def response(contact):
     user_mobile_no = contact
-    #Send sms to user_mobile_no
-    pass
+    # Download the helper library from https://www.twilio.com/docs/python/install
+    # Find your Account SID and Auth Token at twilio.com/console
+    # and set the environment variables. See http://twil.io/secure
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+    
+    message = client.messages \
+                    .create(
+                         body="Thanks for submitting the google form",
+                         from_='+15017122661',
+                         to='contact'
+                     ) 
+    #Chekout https://www.twilio.com/docs/messaging/quickstart/python for more details and docs
     
 @app.route('/webhook', methods=['POST'])
 def webhook():
